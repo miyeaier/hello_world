077aff90b23b4f8481c5ed2a8249b7631008f483
miyesier <miyesier@gmail.com>
2019-09-16 13:59:05 +0200

    use npm to writ Hello world and we can do what do we wirt will retern that

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import Message from './Message';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+ state = {
+   message: 'Hello Craft Acadamy'
+
+  }
+ 
+      inputHandler = (event) =>{
+        this.setState({
+         message: event.target.value
+        })
+      }
+    
+    render(){
+      return (
+        <div>
+          <Message 
+          message={this.state.message} 
+          />
+          <input 
+          onBlur={this.inputHandler}
+          />
+        </div>
+        );
+    }
+    }
 
 export default App;
